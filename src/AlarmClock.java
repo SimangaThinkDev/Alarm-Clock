68ca0f0b5b097004df7f489fbb74a0d19cd25c6b
Simangaliso Phakwe <siphakwjhb024@student.wethinkcode.co.za>
2025-04-21 04:22:41 +0200

    Added comment on AlarmClock class

--- src/AlarmClock.java
+++ src/AlarmClock.java
@@ -6,10 +6,12 @@ import javax.sound.sampled.Clip;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
 
-import java.awt.Toolkit;
 import java.io.File;
 import java.io.IOException;
 
+/*
+ * I think we know what runnable is now, but this is where our other logic lies.
+ */
 public class AlarmClock implements Runnable {
 
     private final LocalTime alarmTime;
@@ -23,7 +25,7 @@ public class AlarmClock implements Runnable {
     @Override
     public void run() {
 
-        // Where our logic runs
+        // Get the current time for comparison with user chosen time...
         LocalTime now = LocalTime.now();
         
         while ( now.isBefore( alarmTime ) ) {
@@ -31,6 +33,8 @@ public class AlarmClock implements Runnable {
             try {
                 Thread.sleep(1000);
 
+                // Using a carriage return to avoid taking up more space to print more time
+                // When we could achieve that in one line...
                 System.out.printf( "\r%02d:%02d:%02d",
                                      now.getHour(), 
                                      now.getMinute(), 
@@ -39,6 +43,7 @@ public class AlarmClock implements Runnable {
             } catch (InterruptedException e) {
                 System.out.println( "Thread was interrupted" );
             }
+            // renew the localTime object that was created
             now = LocalTime.now();
 
         }
@@ -46,20 +51,31 @@ public class AlarmClock implements Runnable {
         System.out.println( "\nAlarm Noises" );
         // Toolkit.getDefaultToolkit().beep(); // This does not work on Mint
 
+        // Use the playsound method to play alarm
         playSound( filePath );
 
     }
 
+    /*
+     * This method is responsible for Playing our Alarm
+     * @param : filePath, String, should be the path to where your desired alarm sound is
+     * NB -> should be a wav file. though the logic for that is handled by the run method
+     */
     private void playSound( String filePath ) {
 
+        // Create a husk file out of the string path provided
         File audioFile   = new File( filePath );
-        try ( AudioInputStream audioStream = AudioSystem.getAudioInputStream(audioFile); ) {
+        // AudioInputStream is in a scanner because it (Like a scanner) needs to be closed
+        try ( AudioInputStream audioStream = AudioSystem.getAudioInputStream(audioFile);
+            // What this does for us is it uses our husk file to get the actual audio out of the object 
+              ) {
+            // Now to use the Clip class so we can effectively play/manipulate the audio file.
             Clip clip = AudioSystem.getClip();
             clip.open( audioStream );
-            clip.start();
+            clip.start(); // Plays the clip
 
             System.out.println("Press enter to stop the alarm!");
-            Main.scanner.nextLine();
+            Main.scanner.nextLine(); // So if I just say this, The program will wait for an [enter] carriage in the terminal
 
         } catch (UnsupportedAudioFileException e) {
             System.out.println( "File not supported" );
@@ -70,6 +86,8 @@ public class AlarmClock implements Runnable {
         catch ( IOException e ) {
             System.out.println( "There was an error" );
         }
+        // As a result of getting used in multiple places, We close the scanner where the
+        // program logic ends
         Main.scanner.close();
     }
 
