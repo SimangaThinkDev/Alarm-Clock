303f33f901b0e0b99421b29f1a5f0ee0b0894811
Simangaliso Phakwe <siphakwjhb024@student.wethinkcode.co.za>
2025-04-21 03:15:56 +0200

    Adding main logic, for parsing user input to a LocalTime Object

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Scanner;
+
+public class Main {
+
+    static Scanner scanner = new Scanner( System.in );
+
+    public static void main(String[] args) {
+        
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern( "HH:mm:ss" );
+        LocalTime alarmTime = null;
+        System.out.print( "Enter an alarm time (HH:MM:SS): " );
+
+        do {
+
+            try {
+
+                String inputTime = scanner.nextLine();
+                
+                alarmTime = LocalTime.parse( inputTime, formatter );
+                System.out.println( "Alarm set for " + alarmTime );
+                
+            }
+            catch ( DateTimeParseException e ) {
+                System.out.print( "Invalid Time, Time should be in format (HH:MM:SS): " );
+            }
+        }
+        while ( alarmTime == null);
+
+        scanner.close();
+    }
+
+}
